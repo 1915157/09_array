a537685d46bcfab33419861754022a169201a921
1915157 <chonsa0409@naver.com>
2019-10-31 15:48:45 +0900

    03

--- main.c
+++ main.c
@@ -6,18 +6,16 @@
 int main(int argc, char *argv[]) {
 	
 	int i;
-	int grade[5]; // 0 1 2 3 4 까지 생성 
-	
-	grade [0] = 10;
-	grade [1] = 20;
-	grade [2] = 30;
-	grade [3] = 40;
-	grade [4] = 50;
+	int a[5] = {1,2,3,4,5};
+	int b[5] = {1,2,3,5,5};
 	
 	for(i=0; i<5; i++)
 	{
-		printf("grade [%d] = %d\n", i, grade[i]);
-	}
+		if (a[i] != b[i])
+		{
+			printf("%i is different\n", i);
+		}
+	 } 
 	
 	return 0;
 }
